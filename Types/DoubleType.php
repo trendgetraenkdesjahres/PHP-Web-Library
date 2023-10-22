3175ed08582efe4e8fc798adaf49101931f8e544
fabian <schwender.fab@gmail.com>
2023-10-22 13:54:40 +0200

    added doubleclass type

---
+++ Types/DoubleType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Types;
+
+/**
+ * DoubleType represents a class for handling integer values.
+ *
+ * @package TypeWrap
+ */
+class DoubleType extends TypeWrap
+{
+    /**
+     * Convert an integer to its string representation.
+     *
+     * @return string The string representation of the integer.
+     */
+    public function __toString(): string
+    {
+        return (string) $this->value;
+    }
+}
