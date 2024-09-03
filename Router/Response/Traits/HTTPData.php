0fa9ee13db160a332a6e63324731d2c009682184
Fabian Schwneder <schwender.fab@gmail.com>
2024-09-03 10:43:38 +0200

    jo

--- Router/Response/Traits/HTTPData.php
+++ Router/Response/Traits/HTTPData.php
@@ -6,26 +6,27 @@ trait HTTPData
 {
     public function articulate(): void
     {
-        $size = filesize($this->path);
-        $time = date('r', filemtime($this->path));
+        $file_size = filesize($this->path);
+        $file_date = date('r', filemtime($this->path));
 
-        $fm = @fopen($this->path, 'rb');
-        if (!$fm) {
+        $file = @fopen($this->path, 'rb');
+        if (!$file) {
             header("HTTP/1.0 505 Internal server error");
             return;
         }
-        $begin = 0;
-        $end = $size;
+
+        $file_position = 0;
+        $file_end = $file_size;
 
         if (isset($_SERVER['HTTP_RANGE'])) {
             if (preg_match('/bytes=\h*(\d+)-(\d*)[\D.*]?/i', $_SERVER['HTTP_RANGE'], $matches)) {
-                $begin = intval($matches[0]);
+                $file_position = intval($matches[0]);
                 if (!empty($matches[1]))
-                    $end = intval($matches[1]);
+                    $file_end = intval($matches[1]);
             }
         }
 
-        if ($begin > 0 || $end < $size)
+        if ($file_position > 0 || $file_end < $file_size)
             header('HTTP/1.0 206 Partial Content');
         else
             header('HTTP/1.0 200 OK');
@@ -34,19 +35,17 @@ trait HTTPData
         header('Cache-Control: public, must-revalidate, max-age=0');
         header('Pragma: no-cache');
         header('Accept-Ranges: bytes');
-        header('Content-Length:' . ($end - $begin));
-        header("Content-Range: bytes $begin-$end/$size");
+        header('Content-Length:' . ($file_end - $file_position));
+        header("Content-Range: bytes $file_position-$file_end/$file_size");
         header("Content-Disposition: inline; filename='" . basename($this->path) . "'");
         header("Content-Transfer-Encoding: binary\n");
-        header("Last-Modified: $time");
+        header("Last-Modified: $file_date");
         header('Connection: close');
 
-        $cur = $begin;
-        fseek($fm, $begin, 0);
-
-        while (!feof($fm) && $cur < $end && (connection_status() == 0)) {
-            print fread($fm, min(1024 * 16, $end - $cur));
-            $cur += 1024 * 16;
+        fseek($file, $file_position, 0);
+        while (!feof($file) && $file_position < $file_end && (connection_status() == 0)) {
+            print fread($file, min(1024 * 16, $file_end - $file_position));
+            $file_position += 1024 * 16;
         }
     }
 }
